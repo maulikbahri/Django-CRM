36c4c6c60e8dbc37e6032bac8469a6e519c1b814
Maulik <maulik.bahri@hotmail.com>
2023-08-28 04:33:26 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'password'
+    
+)
+
+
+# prepare a cursor object
+
+cursorObject = dataBase.cursor()
+
+#create a database
+
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("All Done!")
